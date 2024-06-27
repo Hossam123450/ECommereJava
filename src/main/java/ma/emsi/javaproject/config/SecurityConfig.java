901e9ed4bcc3f5b75acfa006859e20d6621648fe
Hossam Eddine Chahi <hossamchahi77@gmail.com>
2024-06-27 14:57:39 +0100

    salat l7arb m3a login

--- src/main/java/ma/emsi/javaproject/config/SecurityConfig.java
+++ src/main/java/ma/emsi/javaproject/config/SecurityConfig.java
@@ -7,8 +7,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -24,119 +28,32 @@ public class SecurityConfig {
 
 
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http, NoOpPasswordEncoder noOpPasswordEncoder)
-            throws Exception {
+    public AuthenticationManager authenticationManager(HttpSecurity http, NoOpPasswordEncoder noOpPasswordEncoder) throws Exception {
         AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
         authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(noOpPasswordEncoder);
-        return authenticationManagerBuilder.build();
-    }
+        return authenticationManagerBuilder.build();    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
+         http.csrf().disable()
                 .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers(("/login**"), ("/logout**"), ("/register"), ("/contact"), ("/")).permitAll()
+                        .requestMatchers(("/login**"), ("/logout**"), ("/register**"), ("/contact**"), ("/")).permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
-                .formLogin(form -> form.loginPage("/login"))
-                .httpBasic(withDefaults())
-                .build();
+                .formLogin().loginPage("/login").successHandler(successHandler());
+        return http.build();
     }
-
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests((authorize) -> authorize
-//                        .requestMatchers("/admin/**").hasRole("ADMIN")
-//                );
-//        return http.build();
-//    }
-//@Bean
-//public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//    return http
-//            .authorizeHttpRequests((authz) -> authz
-//                            .requestMatchers(new AntPathRequestMatcher("/login**"), new AntPathRequestMatcher("/logout**"), new AntPathRequestMatcher("/register")).permitAll()
-////                      .requestMatchers(new AntPathRequestMatcher("/admin/**")).hasRole("ADMIN")
-//                            .anyRequest().authenticated()
-//            )
-//            .formLogin(form -> form.loginPage("/login")) // Configuration de la page de connexion
-////                .formLogin(withDefaults()) // Utilisation de la configuration par défaut pour la page de connexion
-//            .httpBasic(withDefaults()) // Configuration HTTP Basic avec les options par défaut
-//            .build();
-//}
-
+    @Bean
+    public AuthenticationSuccessHandler successHandler() {
+        return new SimpleUrlAuthenticationSuccessHandler("/product");
+    }
     @SuppressWarnings("deprecation")
     @Bean
     public NoOpPasswordEncoder passwordEncoder() {
-        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
-    }
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();    }
+
 
 }
 
 
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig
-//{
-//    private final CustomUserDetailsService userDetailsService;
-//    private final JwtAuthorizationFilter jwtAuthorizationFilter;
-//    public SecurityConfig(CustomUserDetailsService customUserDetailsService, JwtAuthorizationFilter jwtAuthorizationFilter)
-//    {
-//        this.userDetailsService = customUserDetailsService;
-//        this.jwtAuthorizationFilter = jwtAuthorizationFilter;
-//    }
-//
-//    @Bean
-//    public AuthenticationManager authenticationManager(HttpSecurity http, NoOpPasswordEncoder noOpPasswordEncoder) throws Exception
-//    {
-//        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-//        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(noOpPasswordEncoder);
-//        return authenticationManagerBuilder.build();
-//    }
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
-//    {        http.csrf(csrf -> csrf.disable())
-//            .authorizeRequests()
-//            .requestMatchers("/rest/auth/**")
-//            .permitAll()
-//            .anyRequest()
-//            .authenticated()
-//            .and()
-//            .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//            .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);;
-//        return http.build();
-//    }
-//    @Bean
-//    public NoOpPasswordEncoder passwordEncoder()
-//    {
-//        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
-//    }
-//}
-
-
-
-//http
-//        .authorizeHttpRequests((authz) -> authz
-//        .antMatchers("/public/**").permitAll()
-//                .anyRequest().authenticated()
-//            )
-//                    .formLogin((form) -> form
-//        .loginPage("/login")
-//                .permitAll()
-//            )
-//                    .logout((logout) -> logout
-//        .permitAll()
-//            );
-
-
-//@Bean
-//public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//    return http
-//            .authorizeHttpRequests((auth)->auth.requestMatchers("/login**","/logout**", "/register").permitAll()
-//                    .requestMatchers("/admin/**").hasRole("ADMIN")
-//                    .anyRequest().authenticated())
-//            .formLogin((form)->form.loginPage("/login"))
-//            .formLogin(withDefaults())
-//            .httpBasic(withDefaults()).build();
-//}
--- src/main/java/ma/emsi/javaproject/web/AuthController.java
+++ src/main/java/ma/emsi/javaproject/web/AuthController.java
@@ -1,5 +1,7 @@
 package ma.emsi.javaproject.web;
 import ma.emsi.javaproject.entities.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -15,14 +17,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 @RequestMapping("")
 public class AuthController {
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
     private final UserService userService;
-
     private final AuthenticationManager authenticationManager;
 
     public AuthController(UserService userService, AuthenticationManager authenticationManager) {
+        System.err.println("autconstructor");
         this.userService = userService;
         this.authenticationManager = authenticationManager;
     }
@@ -33,7 +38,26 @@ public class AuthController {
     public String login(){
         return "login";
     }
-
+//    @RequestMapping(value = "/login", method = RequestMethod.POST)
+//    public String performLogin(HttpServletRequest request, HttpServletResponse response,
+//                               @ModelAttribute("user") User user) {
+//        System.err.println("postlogin");
+//        logger.info("userlogin object: {}",user);
+//        try {logger.info("userlogin2 object: {}",user);
+//            Authentication authentication = authenticationManager.authenticate(
+//                    new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
+//
+//            SecurityContext securityContext = SecurityContextHolder.getContext();
+//            securityContext.setAuthentication(authentication);
+//            HttpSession session = request.getSession(true);
+//            session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, securityContext);
+//
+//            return "redirect:/product";
+//        } catch (Exception e) {
+//            logger.error("Authentication failedXXX", e);
+//            return "redirect:/login?error";
+//        }
+//    }
     @RequestMapping(value = "/logout",method = RequestMethod.GET)
     public String logout(HttpServletRequest request){
         HttpSession session = request.getSession();
